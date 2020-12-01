e83ea81f735601e88162cc3208c427fbe0a1ee62
Emeka Allison <emekaallison4@gmail.com>
2020-12-01 11:18:45 +0100

    Add react client

--- be/main.py
+++ be/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import random
+from string import ascii_letters, digits
 
 app = FastAPI()
 
@@ -6,6 +9,26 @@ app = FastAPI()
 async def main():
     return {'message': 'Hello World!'}
 
-@app.get("/{id}")
+@app.get("/get/{id}")
 async def find(id: int):
     return {"id": id}
+
+@app.get("/generate")
+async def getSlug():
+    slug = ''.join(random.choices(ascii_letters + digits, k=7))
+    return {'slug': slug}
+
+@app.get("/generate/{slug}")
+async def getSlug(slug: str):
+    return {'slug': slug}
+
+
+
+@app.get("/redirect")
+async def redirectUrl():
+    return RedirectResponse('https://github.com')
+
+
+@app.post("/generate-url")
+async def shorten_url(longUrl: str):
+    pass
---
+++ fe/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Spit.sh</h1>
+        <p>
+          Create short quick links with custom slugs
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
