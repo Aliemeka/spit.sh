f39535b218ca3c4f1c4e94b012786ece24d0279a
Emeka Allison <emekaallison4@gmail.com>
2020-12-01 20:48:58 +0100

    Created generate endpoint to shorten URLs

--- be/main.py
+++ be/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 import random
 from string import ascii_letters, digits
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class URLData(BaseModel):
+    url: str
+    slug: Optional[str] = None
 
 @app.get("/")
 async def main():
@@ -13,10 +19,12 @@ async def main():
 async def find(id: int):
     return {"id": id}
 
-@app.get("/generate")
-async def getSlug():
-    slug = ''.join(random.choices(ascii_letters + digits, k=7))
-    return {'slug': slug}
+@app.post("/generate")
+async def getSlug(urlData: URLData):
+    if not urlData.slug:
+        urlData.slug = ''.join(random.choices(ascii_letters + digits, k=7))
+    generated_link = f'spit.sh/{urlData.slug}'
+    return {'longUrl': urlData.url, 'slug': urlData.slug, 'generatedLink': generated_link}
 
 @app.get("/generate/{slug}")
 async def getSlug(slug: str):
