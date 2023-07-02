1d0ef14fc8b984589c01c9bafcc2df1f824d1d5d
Emeka Allison <emekaallison4@gmail.com>
2023-07-02 22:38:33 +0100

    Feat: Session crud

---
+++ be/crud/session.py
@@ -0,0 +1,29 @@
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime, timedelta
+import uuid
+
+
+from models.base import Session
+from utils.auth_handlers import generate_token, hash_otp
+
+
+async def get_session_by_token(temp_token: str, session: AsyncSession) -> Session:
+    sessions = await session.execute(
+        select(Session).where(Session.temp_token == temp_token)
+    )
+    user_session: Session | None = sessions.scalars().one_or_none()
+    return user_session
+
+
+async def create_session(email: str, otp: str, user_id: uuid.UUID, db: AsyncSession):
+    temp_token = generate_token()
+    expires = datetime.utcnow() + timedelta(minutes=30)
+    hashed_otp = hash_otp(otp)
+
+    session = Session(
+        temp_token=temp_token, expires=expires, hashed_otp=hashed_otp, user_id=user_id
+    )
+    await db.commit()
+    await db.refresh(session)
+    return session
