e5229629a57af48a5af9b70498934ad7ad56c965
Emeka Allison <emekaallison4@gmail.com>
2026-03-16 18:35:12 +0100

    feat(Input): enhance input component to support dynamic className prop

--- frontend/components/ui/input.tsx
+++ frontend/components/ui/input.tsx
@@ -1,4 +1,5 @@
 import React, { useId } from "react";
+import { cn } from "@/lib/utils";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -11,6 +12,7 @@ const Input: React.FC<InputProps> = ({
   value,
   name,
   label,
+  className,
   onChange,
   ...props
 }) => {
@@ -25,7 +27,10 @@ const Input: React.FC<InputProps> = ({
         name={name}
         value={value}
         onChange={onChange}
-        className='w-full mt-2 px-3 py-2 dark:bg-gray-800 dark:text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg focus:ring-offset-2 focus:ring-offset-transparent focus:ring-1 focus:ring-fuchsia-200 focus:shadow-lg focus:shadow-fuchsia-400/50'
+        className={cn(
+          "w-full mt-2 px-3 py-2 dark:bg-gray-800 dark:text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg focus:ring-offset-2 focus:ring-offset-transparent focus:ring-1 focus:ring-fuchsia-200 focus:shadow-lg focus:shadow-fuchsia-400/50",
+          className,
+        )}
         {...props}
       />
     </div>
